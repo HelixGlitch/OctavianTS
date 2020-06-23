11aace02116e21afee465082ebe7d97ce065aa52
HelixGlitch <56409139+HelixGlitch@users.noreply.github.com>
2020-06-23 14:37:10 +0300

    Delete Config.js
    Deleted some files that are never meant to see the light of day.

--- dist/Config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.owners = exports.prefix = exports.token = void 0;
-exports.token = "NjUwMDczMzE3NzQ2NjcxNjc2.XtutYQ.4x1CuMAr2kUPfJV63eoCNso8VHw";
-exports.prefix = "#";
-exports.owners = [];
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiQ29uZmlnLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vc3JjL0NvbmZpZy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFBVyxRQUFBLEtBQUssR0FBVyw2REFBNkQsQ0FBQztBQUM5RSxRQUFBLE1BQU0sR0FBVyxHQUFHLENBQUM7QUFDckIsUUFBQSxNQUFNLEdBQWEsRUFBRSxDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGxldCB0b2tlbjogc3RyaW5nID0gXCJOalV3TURjek16RTNOelEyTmpjeE5qYzIuWHR1dFlRLjR4MUN1TUFyMmtVUGZKVjYzZW9DTnNvOFZId1wiO1xyXG5leHBvcnQgbGV0IHByZWZpeDogc3RyaW5nID0gXCIjXCI7XHJcbmV4cG9ydCBsZXQgb3duZXJzOiBzdHJpbmdbXSA9IFtdOyJdfQ==
